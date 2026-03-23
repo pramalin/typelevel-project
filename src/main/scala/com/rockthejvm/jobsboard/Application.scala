50179ef9b474a23eb51bef4f4361d15b6166fc31
Padhu Ramalingam <apadhu@yahoo.com>
2026-03-22 22:21:36 -0400

    added configuration

--- src/main/scala/com/rockthejvm/jobsboard/Application.scala
+++ src/main/scala/com/rockthejvm/jobsboard/Application.scala
@@ -17,18 +17,20 @@ object Application extends IOApp.Simple {
 
   given logger: Logger[IO] = Slf4jLogger.getLogger[IO]
   
-  override def run = ConfigSource.default.loadF[IO, EmberConfig].flatMap { config =>
-    val appResource = for {
-      core <- Core[IO]
-      httpApi <- HttpApi[IO](core)
-      server  <- EmberServerBuilder
-                .default[IO]
-                .withHost(config.host)
-                .withPort(config.port)
-                .withHttpApp(httpApi.endpoints.orNotFound)
-                .build
-    } yield server
+  override def run = ConfigSource.default.loadF[IO, AppConfig].flatMap {
+    case AppConfig(postgresConfig, emberConfig) =>
+      val appResource = for {
+        xa <- Database.makePostgresResource[IO](postgresConfig)
+        core <- Core[IO](xa)
+        httpApi <- HttpApi[IO](core)
+        server  <- EmberServerBuilder
+                  .default[IO]
+                  .withHost(emberConfig.host)
+                  .withPort(emberConfig.port)
+                  .withHttpApp(httpApi.endpoints.orNotFound)
+                  .build
+      } yield server
 
-    appResource.use(_ => IO.println("Rock the JVM!") *> IO.never)
+      appResource.use(_ => IO.println("Rock the JVM!") *> IO.never)
   }
 }
--- src/main/scala/com/rockthejvm/jobsboard/modules/Core.scala
+++ src/main/scala/com/rockthejvm/jobsboard/modules/Core.scala
@@ -5,24 +5,16 @@ import cats.implicits.*
 import doobie.hikari.HikariTransactor
 import doobie.util.*
 import com.rockthejvm.jobsboard.core.*
+import doobie.util.transactor.Transactor
+
 
 final class Core[F[_]] private (val jobs: Jobs[F])
 
 // postgres -> jobs -> core -> http api -> app
 object Core {
-  def postgresResource[F[_]: Async]: Resource[F, HikariTransactor[F]] = for {
-    ec <- ExecutionContexts.fixedThreadPool[F](8)
-    xa <- HikariTransactor.newHikariTransactor[F](
-      "org.postgresql.Driver",
-      "jdbc:postgresql:board",
-      "docker",
-      "docker",
-      ec
-    )
-  } yield xa
 
-  def apply[F[_]: Async]: Resource[F, Core[F]] =
-    postgresResource[F]
-      .evalMap(postgres => LiveJobs[F](postgres))
+  def apply[F[_]: Async](xa: Transactor[F]): Resource[F, Core[F]] =
+    Resource
+      .eval(LiveJobs[F](xa))
       .map(jobs => new Core(jobs))
 }
